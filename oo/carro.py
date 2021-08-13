4abbde5f116c0af4bfb6e02b9ff34300dc6a5076
Davi Augusto <davi.cianorte@gmail.com>
2021-08-12 22:52:16 -0300

    carro concluido

--- oo/carro.py
+++ oo/carro.py
@@ -82,22 +82,42 @@ A direção terá a responsabilidade de controlar a direção. ela oferece os se
     >>> carro.calcular_velocidade()
     0
     >>> carro.calcular_direcao()
-    >>> 'Norte'
+    'Norte'
     >>> carro.girar_a_direita()
     >>> carro.calcular_a_direcao()
-    >>> 'Leste'
+    'Leste'
     >>> carro.girar_a_esquerda()
     >>> carro.calcular_a_direcao()
-    >>> 'Norte'
+    'Norte'
     >>> carro.girar_a_esquerda()
     >>> carro.calcular_a_direcao()
-    >>> 'Oeste'
+    'Oeste'
 '''
 
-NORTE='Norte'
-SUL='Sul'
-LESTE='Leste'
-OESTE='Oeste'
+
+class Carro:
+    def __init__(self, direcao, motor):
+        self.motor = motor
+        self.direcao = direcao
+
+    def calcular_velocidade(self):
+        return self.motor.velocidade
+
+    def acelerar(self):
+        self.motor.acelerar()
+
+    def frear(self):
+        self.motor.frear()
+
+    def calcular_direcao(self):
+        return self.direcao.valor
+
+    def girar_a_direita(self):
+        self.direcao_a_direita()
+
+    def girar_a_esquerda(self):
+        self.direcao_a_esquerda()
+
 
 class Direcao:
     rotacao_a_direita_dct = {
